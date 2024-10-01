f278eb6e1dccbac602faf9867dddbc9118c6709b
masha-a-m <escritoramaree@gmail.com>
2024-10-02 05:19:15 +0800

    initial commit

--- src/Components/Homepage/Hero-Homepage.jsx
+++ src/Components/Homepage/Hero-Homepage.jsx
@@ -18,7 +18,7 @@ import profileImage from '../assets/profile img.webp';
 
 const Hero = () => {
   return (
-    <section className="mt-40">
+    <section className="mt-20">
       <div className="flex flex-col md:flex-row items-center justify-center md:mx-auto lg:space-y-8 lg:flex-row lg:space-x-12 py-10 ">
         
         {/* Left Side */}
